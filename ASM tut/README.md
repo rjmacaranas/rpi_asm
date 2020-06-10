b01261f95c6e3114a1d5758e10108f0995f1538c
rjmacaranas <rjmac22899@gmail.com>
2020-06-10 10:41:34 -0600

    Update README

--- ASM tut/README.md
+++ ASM tut/README.md
@@ -8,5 +8,40 @@ ld -o asmtut asmtut.o
 ```
 
 as - o asmtut.o asmtut.s assembles the program
+
+
 ld -o asmtut asmtut.o creates an executable
+
+
+
 ./asmtut ; echo $? runs the executable and outputs the value set for R0 register
+
+
+To execute and merge source files
+
+```shell
+ld -o asmtut4 asmtut2.o asmtut3.o
+./asmtut4 ; echo $?
+```
+
+or create a make file
+```
+vim makefile
+target: prerequisite
+    recipe
+clean:
+    recipe
+:wq
+make clean
+```
+
+ARM is a 32 bit CPU
+- R0-12 are
+- R13 is a pointer to the active stack
+- R14 is the link register (for subroutines)
+- R15 is the program counter
+- R31 goes high for negative
+- R30 goes high for zero
+- R29 goes high for carry
+- R28 goes high for overflow
+- R8-27 are general purpose
--- ASM tut/asmtut.s
+++ ASM tut/asmtut.s
@@ -1,9 +1,25 @@
-.text
-
 .global _start              @ make start globally available to program
 
-_start:                     @ label
-    MOV R0, #65             @ store numbers in registers
-    MOV R7, #1              @ system call number
+@ <instruction> <Dest>, <Operand>, <Operand>
 
-SWI 0                       @ software interrupt, exits to terminal
+_start:                     @ label
+    MOV R7, #3              @ system call = 4 > output to screen 
+    MOV R0, #1              @ output to monitor
+    MOV R2, #10             @ define length of string to put on screen
+    LDR R1, =message        @ load message
+    SWI 0                   @ software interrupt, exits to terminal
+
+_write:
+    MOV R7, #4              @ system call = 4 > output to screen 
+    MOV R0, #1              @ output to monitor
+    MOV R2, #5             @ define length of string to put on screen
+    LDR R1, =message        @ load message
+    SWI 0                   @ software interrupt, exits to terminal
+
+end:
+    MOV R7, #1              @ jump to terminal
+    SWI 0
+
+.data
+message:
+    .ascii " "
